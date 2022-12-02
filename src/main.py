33387e1030ad72659d772f0f9f15aa7a195e4e65
Freshrojek <jnboyer1@gmail.com>
2022-12-02 13:19:20 +0000

    cleaned functions up

--- src/main.py
+++ src/main.py
@@ -1,24 +1,33 @@
 import sys
 
+
+def get_data():
+    file_data, total_participants = read_file()
+    tournament_participants = get_participants(file_data, total_participants)
+    tournament_weighting = get_weighting(file_data, total_participants)
+
+
 def read_file():
     file = open(sys.argv[1])
     file_data = file.read().splitlines()
-    return file_data
-
-def get_participants(file_data):
     total_participants = file_data[0]
+    return file_data, total_participants
+
+
+def get_participants(file_data, total_participants):
     participants = {}
     for i in range(int(total_participants)):
-        id, name = file_data[i+1].split(",")
+        id, name = file_data[i + 1].split(",")
         participants[id] = name
+    return participants
 
-def get_weighting():
+
+def get_weighting(file_data, total_participants):
     tournament_weighting = {}
     for i in range(int(total_participants) + 2, len(file_data)):
         weight, participant_A, participant_B = file_data[i].split(",")
-        tournament_weighting[(participant_A,participant_B)] = weight
+        tournament_weighting[(participant_A, participant_B)] = weight
+    return tournament_weighting
 
 
-
-
-read_file()
+get_data()
