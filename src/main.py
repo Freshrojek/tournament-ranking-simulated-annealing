6f21ca5513a2bab4bb8d8bb8ad675518627fb05c
Freshrojek <jnboyer1@gmail.com>
2022-12-06 09:51:30 +0000

    added function to calculate kemeny score/cost

--- src/main.py
+++ src/main.py
@@ -59,58 +59,80 @@ def get_random_neighbouring_ranking(current_ranking):
     start_edge_A, end_edge_A = get_first_random_edge(random.random(), current_ranking)
     sub_list_A = current_ranking[0:start_edge_A - 1]
     sub_list_B = current_ranking[end_edge_A + 1:len(current_ranking)]
-    print(f"start edge a, {start_edge_A}")
-    print(f"end edge a, {end_edge_A}")
-    print(f"sublist a, {sub_list_A}")
-    print(f"sublist b, {sub_list_B}")
+    # print(f"start edge a, {start_edge_A}")
+    # print(f"end edge a, {end_edge_A}")
+    # print(f"sublist a, {sub_list_A}")
+    # print(f"sublist b, {sub_list_B}")
 
     remaining_participants = sub_list_A + sub_list_B
-    print(remaining_participants)
+    # print(remaining_participants)
     start_edge_B, end_edge_B = get_second_random_edge(random.random(), remaining_participants, current_ranking)
     end_edge_B_value = current_ranking[start_edge_B]
-    print(f"start edge b, {start_edge_B}")
-    print(f"end edge b, {end_edge_B}")
+    # print(f"start edge b, {start_edge_B}")
+    # print(f"end edge b, {end_edge_B}")
     sub_list_C = remaining_participants[0:start_edge_B - 1]
     sub_list_D = remaining_participants[end_edge_B:len(remaining_participants)]
-    print(sub_list_C + sub_list_D)
+    # print(sub_list_C + sub_list_D)
     sub_tour = []
-    print(f"index1test {current_ranking.index(current_ranking[start_edge_A])}")
-    print(f"index2test {current_ranking.index(remaining_participants[end_edge_B - 1])}")
+    sub_tour_start = 0
+    sub_tour_end = len(current_ranking) - 1
+    # print(f"index1test {current_ranking.index(current_ranking[start_edge_A])}")
+    # print(f"index2test {current_ranking.index(remaining_participants[end_edge_B - 1])}")
 
     if int(current_ranking.index(current_ranking[start_edge_A])) > int(
             current_ranking.index(remaining_participants[end_edge_B - 1])):
+        sub_tour_start = int(current_ranking.index(remaining_participants[end_edge_B - 1]))
+        sub_tour_end = int(current_ranking.index(current_ranking[start_edge_A]))
         sub_tour = current_ranking[int(current_ranking.index(remaining_participants[end_edge_B - 1])):int(
             current_ranking.index(current_ranking[start_edge_A]))]
-        sub_tour.insert(int(current_ranking.index(current_ranking[end_edge_A-1])), current_ranking[end_edge_A-1])
-        sub_tour.insert(int(
-            current_ranking.index(remaining_participants[start_edge_B - 1])), remaining_participants[start_edge_B - 1])
+
     else:
+        sub_tour_start = int(current_ranking.index(current_ranking[end_edge_A - 1]))
+        sub_tour_end = int(
+            current_ranking.index(remaining_participants[start_edge_B - 1]))
         sub_tour = current_ranking[int(current_ranking.index(current_ranking[end_edge_A - 1])):int(
             current_ranking.index(remaining_participants[start_edge_B - 1]))]
-        sub_tour.insert(int(current_ranking.index(remaining_participants[end_edge_B - 1])),
-                        remaining_participants[end_edge_B - 1])
-        sub_tour.insert(int(current_ranking.index(current_ranking[start_edge_A])),
-                        current_ranking.index(current_ranking[start_edge_A]))
-    sub_tour.reverse()
-    print(f"reversed sub tour = {sub_tour}")
 
-    print(current_ranking)
+    # print(f"sub tour = {sub_tour}")
+    sub_tour.reverse()
+    # print(f"reversed sub tour = {sub_tour}")
+    current_ranking[sub_tour_start:sub_tour_end] = sub_tour
+    # print(current_ranking)
 
     if ((start_edge_A == 0) and (end_edge_A == 0)) or ((start_edge_B == 0) and (end_edge_B == 0)):
         print("Error, both edges are 0")
+    return current_ranking
+
+
+def get_cost(tournament_participants, tournament_weighting, ranking):
+    cost = 0
+    for matchup, weighting in tournament_weighting.items():
+        for i in range(len(ranking)):
+            for j in range(i+1, len(ranking)):
+                if matchup[0] == ranking[j] and matchup[1] == ranking[i]:
+                    print(f"{ranking[j]} beat {ranking[i]} by {weighting}")
+                    cost = cost + int(weighting)
+    print(cost)
+    return cost
 
 
 def simulated_annealing_algorithm():
-    # tournament_participants, tournament_weighting = get_data()
-    tournament_participants = {"A": "Alice", "B": "bettie", "C": "Charlie", "D": "Dan", "E": "Ellie", "F": "Fred",
-                               "G": "Gina", "H": "Harry"}
+    tournament_participants, tournament_weighting = get_data()
+    # tournament_weighting = {("B", "C"): "1", ("D", "E"): "6", ("G", "F"): "3", ("B", "A"): "2", ("D", "E"): "4",
+    #                         ("A", "B"): "9", ("F", "A"): "6"}
+    # tournament_participants = {"A": "Alice", "B": "bettie", "C": "Charlie", "D": "Dan", "E": "Ellie", "F": "Fred",
+    #                            "G": "Gina", "H": "Harry"}
     temperature_length = 10
     cooling_ration = 0.95
     num_non_improve = 8000
     loops_without_optimal_solution = 0
-    initial_ranking = [i for i in tournament_participants]
+    current_ranking, initial_ranking = [i for i in tournament_participants], [i for i in tournament_participants]
+    # get_random_neighbouring_ranking(initial_ranking, previous_ranking)
+    cost = get_cost(tournament_participants, tournament_weighting, initial_ranking)
+    # cost = get_cost(tournament_participants, tournament_weighting, previous_cost)
     for i in range(int(temperature_length)):
-        neighbouring_ranking = get_random_neighbouring_ranking(initial_ranking)
+        cost = get_cost(tournament_participants, tournament_weighting, current_ranking)
+        neighbouring_ranking = get_random_neighbouring_ranking(current_ranking)
 
 
 simulated_annealing_algorithm()
